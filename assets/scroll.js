3c190d69a28dec71f2fff25ab76aafe024da1767
whoisalexico <axux228@gmail.com>
2023-09-22 20:35:31 +0300

    test

---
+++ assets/scroll.js
@@ -0,0 +1,25 @@
+/*
+let mission = document.getElementsByClassName("mission");
+let services = document.getElementsByClassName("services");
+let team = document.getElementsByClassName("team");
+
+
+
+let header_mission = document.getElementsByClassName("Mission");
+let header_services = document.getElementsByClassName("Services");
+let header_team = document.getElementsByClassName("Team");
+
+
+let mission_top = mission.offsetTop;
+let services_top = services.offsetTop;
+let team_top = team.offsetTop;
+console.log(header_mission)
+
+
+header_mission.onclick = ()=>{
+    console.log(mission_top)
+    window.scrollTo({
+        top: mission_top,
+        behavior:"smooth"
+    })
+}*/
